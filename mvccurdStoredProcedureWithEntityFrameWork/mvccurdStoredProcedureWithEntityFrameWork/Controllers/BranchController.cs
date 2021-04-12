90b85d4514eba41e226e1091b97b84915b515fec
Vignesh N <vignesh.n@NATL.COM>
2021-04-12 15:35:51 +0530

    12/04/21

---
+++ mvccurdStoredProcedureWithEntityFrameWork/mvccurdStoredProcedureWithEntityFrameWork/Controllers/BranchController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvccurdStoredProcedureWithEntityFrameWork.Models;
+
+namespace mvccurdStoredProcedureWithEntityFrameWork.Controllers
+{
+    public class BranchController : Controller
+    {
+        databaseEntities db = new databaseEntities();
+        // GET: Branch
+        public ActionResult Index()
+        {
+          
+            return View(db.sp_select_branch().ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Branch o)
+        {
+
+
+           
+                db.sp_insert_branch(o.BranchId, o.BranchName);
+  
+                return View();
+          
+        }
+
+        public ActionResult Edit(int id)
+        {
+          var s=  db.sp_find_branch(id).First();
+            return View(s);
+        }
+        [HttpPost]
+        public ActionResult Edit(Branch p)
+        {
+           db.sp_update_branch(p.BranchId, p.BranchName);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id )
+        {
+            //db.sp_delete_branch(id);
+            //return RedirectToAction("Index");
+
+            var res = db.Branches.Where(x => x.BranchId == id).First();
+            db.Branches.Remove(res);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ mvcusingWithEntityFrameWork/mvcusingWithEntityFrameWork/Controllers/StudentController.cs
@@ -0,0 +1,89 @@
+﻿using mvcusingWithEntityFrameWork.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcusingWithEntityFrameWork.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        db_testEntities dbObj = new db_testEntities();
+        // GET: Student
+        public ActionResult Student(tbl_student obj)
+        {
+            //if (obj != null)
+            //{
+            //    return View(obj);
+            //}
+            //else { return View(); }
+            return View(obj);
+
+
+        }
+
+        //public ActionResult Student()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public ActionResult AddStudent(tbl_student model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                tbl_student obj = new tbl_student();
+
+                /////////////
+                obj.ID = model.ID;
+                ///
+                obj.Name = model.Name;
+                obj.Fname = model.Fname;
+                obj.Email = model.Email;
+                obj.Mobile = model.Mobile;
+                obj.Description = model.Description;
+
+
+                ///////
+                ///
+
+                if (model.ID == 0)
+                {
+                    dbObj.tbl_student.Add(obj);
+                    dbObj.SaveChanges();
+                }
+                ///
+                else
+                {
+                    dbObj.Entry(obj).State = System.Data.Entity.EntityState.Modified;
+                    dbObj.SaveChanges();
+                }
+               
+
+            }
+            ModelState.Clear();
+            return View("Student");
+        }
+
+        public ActionResult StudentList()
+        {
+            var res = dbObj.tbl_student.ToList();
+            return View(res);
+        }
+        public ActionResult Delete(int id)
+        {
+            var res = dbObj.tbl_student.Where(x => x.ID == id).First();
+            dbObj.tbl_student.Remove(res);
+            dbObj.SaveChanges();
+
+            var reslist = dbObj.tbl_student.ToList();
+            return View("StudentList", reslist);
+       
+        }
+
+
+    }
+}
